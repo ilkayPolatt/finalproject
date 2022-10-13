96b4a9bc2641ffa670bc3089d798f84a39a8ee27
PLT <suwari6345@gmail.com>
2022-10-13 11:32:24 +0300

    ...

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_finalproje/view/doctor_info.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: DoctorInfo(),
+    );
+  }
+}
